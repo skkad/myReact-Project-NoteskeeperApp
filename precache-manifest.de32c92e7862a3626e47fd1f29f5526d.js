a2dcf1cc337e7496f69a0ce17dcb48e4a9e08cf4
skkad <skkad1998@gmail.com>
2022-08-22 14:09:41 +0530

    Updates

---
+++ precache-manifest.de32c92e7862a3626e47fd1f29f5526d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f44afdad7cce92bb5497e89c60501e",
+    "url": "/myReact-Project-NoteskeeperApp/index.html"
+  },
+  {
+    "revision": "766483f1daca33c3e5b9",
+    "url": "/myReact-Project-NoteskeeperApp/static/js/2.ae7643cb.chunk.js"
+  },
+  {
+    "revision": "25acd59fe8010d13afab",
+    "url": "/myReact-Project-NoteskeeperApp/static/js/main.23ab1844.chunk.js"
+  },
+  {
+    "revision": "3ced30f22647cb549e80",
+    "url": "/myReact-Project-NoteskeeperApp/static/js/runtime-main.ae8059ba.js"
+  }
+]);
